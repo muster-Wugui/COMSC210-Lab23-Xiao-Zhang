a485868056892185d82bd318a8b7aaffc3c43d8c
muster-Wugui <tintinpost28@gmail.com>
2024-10-26 13:17:35 -0700

    Initial Commit

--- COMSC210-Lab23-Xiao Zhang/main.cpp
+++ COMSC210-Lab23-Xiao Zhang/main.cpp
@@ -6,9 +6,60 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <list>
+#include "Goat.h"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
+
+int select_goat(list<Goat> trip);
+void delete_goat(list<Goat> &trip);
+void add_goat(list<Goat> &trip, string [], string []);
+void display_trip(list<Goat> trip);
+int main_menu();
+
+int main() {
+    srand(time(0));
+    bool again;
+
+    // read & populate arrays for names and colors
+    ifstream fin("names.txt");
+    string names[SZ_NAMES];
+    int i = 0;
+    while (fin >> names[i++]);
+    fin.close();
+    ifstream fin1("colors.txt");
+    string colors[SZ_COLORS];
+    i = 0;
+    while (fin1 >> colors[i++]);
+    fin1.close();
+
+
+
+
     return 0;
+}
+
+int main_menu(){
+    int choice = 0;
+    cout<<"*** GOAT MANAGER 3001 ***"<<endl;
+    cout<<"[1] Add a goat"<<endl;
+    cout<<"[2] Delete a goat"<<endl;
+    cout<<"[3] List goats"<<endl;
+    cout<<"[4] Quit"<<endl;
+    cout<<"Choice --> ";
+    
+    cin>>choice;
+    
+    return choice;
+}
+
+void display_trip(list<Goat> trip){
+    for(char it = trip.begin(); it != trip.end(); ++it){
+        cout<<*it<<endl;
+    }
+    
+    
 }
